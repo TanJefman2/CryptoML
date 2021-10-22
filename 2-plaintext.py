4c11cb2cd28da6b35d5349b52db9fae9363eae00
TanJefman2 <91386880+TanJefman2@users.noreply.github.com>
2021-10-22 11:58:00 +0800

    Added references

--- 2-plaintext.py
+++ 2-plaintext.py
@@ -185,7 +185,7 @@ def real_differences_data(n, nr, diff=(0x0040,0)):
 ##########################################################################################
 
 ################################# GOHR'S TRAIN_NETS.PY ###########################################
-# CODE RETRIEVED FROM: https://github.com/agohr/deep_speck/blob/master/speck.py
+# CODE RETRIEVED FROM: https://github.com/agohr/deep_speck/blob/master/train_nets.py
 
 def cyclic_lr(num_epochs, high_lr, low_lr):
   res = lambda i: low_lr + ((num_epochs-1) - i % num_epochs)/(num_epochs-1) * (high_lr - low_lr);
